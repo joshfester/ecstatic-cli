9019b6be34d8f700322520b9950d745af31eb2d0
Josh Fester <joshfester@gmail.com>
2025-09-12 16:11:14 +0000

    fix siteone install

---
+++ packages/siteone/src/Crawler/Version.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of the SiteOne Crawler.
+ *
+ * (c) Ján Regeš <jan.reges@siteone.cz>
+ */
+
+declare(strict_types=1);
+
+namespace Crawler;
+
+abstract class Version
+{
+    const CODE = '1.0.9.20250608';
+}
