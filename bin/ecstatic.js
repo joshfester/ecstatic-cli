99b264595ce4f152f38c5abdcab460eca7d85c36
Josh Fester <joshfester@gmail.com>
2025-09-21 05:13:55 +0000

    update jampack

--- bin/ecstatic.js
+++ bin/ecstatic.js
@@ -15,7 +15,7 @@ if (isProductionEnvironment()) {
 
 // Hardcode package info for Bun compatibility
 const packageJson = {
-  version: "1.0.5",
+  version: "1.0.6",
   description:
     "CLI tool for website optimization - download, optimize, and deploy static sites",
 };
